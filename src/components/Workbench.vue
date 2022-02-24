91bff34e315b51caccc3fb30813b02624f14f092
Mark Saccomano <mss2221@columbia.edu>
2022-02-24 17:47:20 +0100

    begin Workbench

--- src/components/Workbench.vue
+++ src/components/Workbench.vue
@@ -1,15 +1,54 @@
 <template>
   <div class="hello">
-    <h1>Workbench</h1>
-    <div>This is the Workbench.</div>
-      <splitpanes style="height: 600px" class="default-theme">
-        <pane>
-          <h2>Upper Pane [0]</h2>
+    <splitpanes vertical style="height:700px" class="default-theme">
+
+      <pane >
+        <splitpanes horizontal class="default-theme">
+            <pane >
+              <h4>Upper Pane Selection [0]</h4>
+            </pane>
+            <pane>
+              <h4>Lower Pane Selection [1]</h4>
+            </pane>
+          </splitpanes>
+          </pane>
+
+          <pane size="30">
+            <splitpanes horizontal>
+              <pane size="5">
+            <h3 class="title">Workbench</h3>
+              </pane>
+          <pane size="6">selection 1 info</pane>
+          <pane size="6">selection 2 info</pane>
+          <pane size="20">
+            <div class="controls">
+            <div class="title">Control Panel</div>
+                <div>
+                  <button>Make Selection</button>
+                  <button>Define Parallel Passage</button>
+                </div>
+
+              <div>Annotation type:
+                <div>
+                  <button>describing</button>
+                  <button>commenting</button>
+                </div>
+              </div>
+          </div>
+          </pane>
+
+          <pane size="63">
+
+            <div class="textBox">
+              <h2 class="title">Text Box</h2>
+            </div>
+
+          </pane>
+
+            </splitpanes>
+          </pane>
         </pane>
-        <pane>
-          <h2>Lower Pane [1]</h2>
-        </pane>
-      </splitpanes>
+        </splitpanes>
     </div>
 </template>
 
@@ -27,5 +66,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.title {
+  font-weight: bold;
+}
+.controls {
+  background-color: #9fc2b2;
+}
+.textBox {
+  background-color: #ebd999;
+}
 </style>
